925ce576214f5f4088b231d8e449c2c6b53e0be7
Abhishek Mukhedkar <abhishek.mukhedkar@happiestminds.com>
2020-01-21 12:51:16 +0530

    First POC Automation

---
+++ project/script.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import os
+import subprocess
+
+def main():
+#	os.system('rm -rf main.py')
+#	os.system('git clone https://github.com/avhadutgadhave/Sagemaker_Data.git')
+#	os.system('ipython nbconvert --to python Sagemaker_Data/main.ipynb')
+#	os.system('cp Sagemaker_Data/main.py .')
+	os.system('python3 run.py')
+
+if __name__=='__main__':
+	main()
