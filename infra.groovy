4e68e1989777bb8fd1ee3c35012d48f16a547d0f
abhishekm8669 <57748355+abhishekm8669@users.noreply.github.com>
2020-02-04 16:08:24 +0530

    Update infra.groovy

--- infra.groovy
+++ infra.groovy
@@ -4,22 +4,11 @@ node("master")
   {
   
       git url: 'https://github.com/abhishekm8669/poc_automation.git'
-/*	  sh '''
-	        gcloud
+	  sh '''
+	        gcloud container clusters get-credentials standard-cluster-1 --zone us-central1-a --project kube-demo-261605
 	'''	  
   }
   
-  stage("Configuring EFK")
-  {
-     sh '''
-	       kubectl create -f EFK/elasticsearch.yaml
-		   kubectl create -f EFK/fluentd-rbac.yaml
-		   kubectl create -f EFK/fluentd-daemonset.yaml
-		   kubectl create -f EFK/kibana.yaml
-	 ''' */
-	 
-  }
-	
   stage("Building and Pushing the image")
   {
      sh 'sudo docker build -t mlops-web-app ./project/'        //building an Image using Dockerfile
@@ -35,10 +24,56 @@ node("master")
   stage("Pulling image from DockerHub")
   {
      sh '''
-	       sed -i 's/latest/'$BUILD_NUMBER'/g' Mlops/deploy.yaml
+	       sed -i 's/latest/latest1/g' Mlops/deploy.yaml
 	'''
   
   }
+ 
+  stage("Configuring EFK and Deploying Application")
+  {
+     sh '''
+	       kubectl create -f Mlops/deploy.yaml
+	       kubectl create -f EFK/elasticsearch.yaml
+		   kubectl create -f EFK/fluentd-rbac.yaml
+		   kubectl create -f EFK/fluentd-daemonset.yaml
+		   kubectl create -f EFK/kibana.yaml
+		   sleep 10
+		   
+		   kubectl get pods
+		   kubectl get svc
+	 ''' 
+	 
+  }
   
+  stage("Configuring Prometheus and Grafana")
+  {
+     sh '''
+	       kubectl create namespace monitoring
+		   kubectl create -f prometheus-rbac.yaml
+		   kubectl create -f config-map.yaml
+		   kubectl create -f prometheus-deployment.yaml
+		   kubectl create -f prometheus-service.yaml
+		   
+		   /* Create State Metrics for Kubernetes Cluster */
+		   
+		   kubectl create -f state-metrics-rbac.yaml
+		   kubectl create -f state-metrics-deploy.yaml
+		   kubectl create -f state-metrics-service.yaml
+		   sleep 10
+		   
+		   kubectl get pods -n kube-system | grep metrics
+		   
+		   /* Create Deployment for Grafana */
+		   
+		   kubectl create -f grafana-datasource-config.yaml
+		   kubectl create -f grafana.yaml
+		   sleep 10
+		   
+		   kubectl get pods -n monitoring
+		   kubectl get svc -n monitoring
+		   
+    '''
   
+  }
+    
 }
