baa92408865686a8cedbe894a3a5f0cd72613910
Abhishek Mukhedkar <abhishek.mukhedkar@happiestminds.com>
2020-02-04 14:49:41 +0530

    Infra jenkins File

---
+++ infra.groovy
@@ -0,0 +1,32 @@
+node("master")
+{
+  stage("Connecting to Kubernetes Cluster")
+  {
+  
+      git url: 'https://github.com/abhishekm8669/poc_automation.git'
+/*	  sh '''
+	        gcloud
+	'''	  
+  }
+  
+  stage("Configuring EFK")
+  {
+     sh '''
+	       kubectl create -f EFK/elasticsearch.yaml
+		   kubectl create -f EFK/fluentd-rbac.yaml
+		   kubectl create -f EFK/fluentd-daemonset.yaml
+		   kubectl create -f EFK/kibana.yaml
+	 ''' */
+	 
+  }
+  
+  stage("Pulling image from DockerHub")
+  {
+     sh '''
+	       sed -i 's/latest/latest1/g' Mlops/deploy.yaml
+	'''
+  
+  }
+  
+  
+}
